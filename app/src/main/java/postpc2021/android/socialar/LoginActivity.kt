c9941554aabea43e1187f157476098d59b821dda
Yarin <yarin.schiller@mail.huji.ac.il>
2021-09-28 23:12:43 +0300

    few fixes- new message background

--- app/src/main/java/postpc2021/android/socialar/LoginActivity.kt
+++ app/src/main/java/postpc2021/android/socialar/LoginActivity.kt
@@ -14,6 +14,7 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 import com.mapbox.mapboxsdk.maps.MapView
+import postpc2021.android.socialar.arComponents.ARView
 
 class LoginActivity : AppCompatActivity() {
     private val fireBaseManager = FirebaseWrapper.getInstance().fireBaseManager
