bf0360d8cbf55130618a5e9595c0c0da6571e94c
Yanir Elfassy <yanir.elfassy@mail.huji.ac.il>
2021-10-07 12:05:07 +0300

    add like option

--- app/src/main/java/postpc2021/android/socialar/MessageActionsActivity.kt
+++ app/src/main/java/postpc2021/android/socialar/MessageActionsActivity.kt
@@ -2,12 +2,14 @@ package postpc2021.android.socialar
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import postpc2021.android.socialar.dataTypes.MessageData
 import postpc2021.android.socialar.dataTypes.PostData
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -22,15 +24,37 @@ class MessageActionsActivity : AppCompatActivity() {
 
 		val extras = intent.extras
 		firebase.getPostDetailsFromMessage(extras!!.getString("id"), ::setMessageData)
+		firebase.isUserLikeMessage(firebase.getUserID(), extras.getString("id")!!, ::setLikeButtonStatus)
 
+	}
+
+	fun setLikeButtonStatus(isLikePressed: Boolean, messageID: String): Unit {
+		val likeButton = findViewById<ImageButton>(R.id.likeButton)
+		if(isLikePressed){
+			likeButton.setImageResource(R.drawable.ic_dislike_light)
+			likeButton.setOnClickListener {
+				firebase.dislikeMessage(firebase.getUserID(), messageID, ::setLikeButtonStatus)
+			}
+		}
+		else{
+			likeButton.setImageResource(R.drawable.ic_like_empty)
+			likeButton.setOnClickListener {
+				firebase.likeMessage(firebase.getUserID(), messageID, ::setLikeButtonStatus)
+			}
+		}
 	}
 
 	@RequiresApi(Build.VERSION_CODES.O)
 	fun setMessageData(data: PostData): Unit{
+		val date = SimpleDateFormat("dd-MM-yyyy").parse(data.creationDate)
+		val formattedDate = SimpleDateFormat("dd/MM/yyy").format(date!!)
 		findViewById<TextView>(R.id.userName).text = data.userName
 		findViewById<TextView>(R.id.likes).text = "${data.likeID.size} Likes"
-		findViewById<TextView>(R.id.date).text = LocalDate.parse(data.creationDate, DateTimeFormatter.ISO_DATE).toString()
+		findViewById<TextView>(R.id.date).text = formattedDate.toString()
 		findViewById<TextView>(R.id.messageContent).text = data.textContent
+		if(data.mediaContent.size > 0){
+			DownloadImageTask(findViewById(R.id.imagePlace)).execute(data.mediaContent[0])
+		}
 		DownloadImageTask(findViewById<ImageView>(R.id.profilePicture))
 				.execute(data.profilePicture);
 	}
